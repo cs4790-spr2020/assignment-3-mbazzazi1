d68f8e269737cdc073759cecb91d6f570482b8dd
mbazzazi1 <miladbazzazi@mail.weber.edu>
2020-03-11 22:00:40 -0600

    Add files via upload

---
+++ BlabberApp.DataStore/InMemory.cs
@@ -0,0 +1,70 @@
+﻿using BlabberApp.Domain.Entities;
+using BlabberApp.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlabberApp.DataStore
+{
+    public class InMemory<T> : IRepository<T> where T : BaseEntity
+    {
+        private ApplicationContext Context;
+        private DbSet<T> _entities; 
+
+        public InMemory(ApplicationContext context)
+        {
+            Context = context;
+            _entities = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Add(entity);
+            Context.SaveChanges();
+        }
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            _entities.Remove(entity);
+            Context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            Context.SaveChanges();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+        public T GetBySysId(string sysId)
+        {
+            if (sysId.Equals(""))
+            {
+                throw new ArgumentNullException("sysId");
+            }
+
+            return _entities.SingleOrDefault(s => s.getSysId() == sysId);
+        }
+
+        public T GetByUserId(string userId)
+        {
+            if ( userId.Equals(""))
+            {
+                throw new ArgumentNullException("userId");
+            }
+            return _entities.Find(userId);
+        }
+    }
+}
